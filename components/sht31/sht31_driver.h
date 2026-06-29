2db06db457064a0a196dfd20ecf933e39c6e0c2d
medoxee <medoxee99@gmail.com>
2026-06-29 03:26:02 +0100

    bug fix

--- components/sht31/sht31_driver.h
+++ components/sht31/sht31_driver.h
@@ -50,6 +50,6 @@
 uint8_t	sht31_init(void);
 uint8_t	sht31_read_data(int16_t	*scaled_data);
 uint8_t	sht31_verif_crc(uint8_t	data_msb, uint8_t	data_lsb);
-void	*sht31_convert(int16_t	scaled_data, uint16_t	temp_msb,
+void	sht31_convert(int16_t	*scaled_data, uint16_t	temp_msb,
 		uint16_t	temp_lsb, uint16_t	hum_msb,
 		uint16_t	hum_lsb);
